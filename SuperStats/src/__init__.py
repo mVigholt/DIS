6a86d92eb16c7b69171ae80c45050bcf23766833
mVigholt <zwc822@alumni.ku.dk>
2024-06-04 18:44:16 +0200

    Automate table creation

--- SuperStats/src/__init__.py
+++ SuperStats/src/__init__.py
@@ -8,6 +8,47 @@ from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 
+init = os.getenv('DB_INIT')
+yes = ['1', 'true', 'True', 'TRUE', 'y', 'Y','yes', 'Yes', 'YES']
+if init in yes:
+    # ---------------------------------------------------------------
+    # Create Database
+    # ---------------------------------------------------------------
+    conn1 = psycopg2.connect(
+        host="localhost",
+        database=os.getenv('DB_USERNAME'),
+        user=os.getenv('DB_USERNAME'),
+        password=os.getenv('DB_PASSWORD')
+    )
+    
+    conn1.autocommit = True  # Enable autocommit mode for database creation
+    cur = conn1.cursor()
+    # Check if the 'DB_NAME' database exists
+    cur.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{}'".format(os.getenv('DB_NAME')))
+    exists = cur.fetchone()
+    # If the database does not exist, create it
+    if not exists:
+        cur.execute('CREATE DATABASE "{}"'.format(os.getenv('DB_NAME')))
+    cur.close()
+    conn1.close()
+    
+    # ---------------------------------------------------------------
+    # Run database schema
+    # ---------------------------------------------------------------
+    conn2 = psycopg2.connect(
+        host="localhost",
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USERNAME'),
+        password=os.getenv('DB_PASSWORD')
+    )
+    
+    with conn2.cursor() as cursor:
+        cursor.execute(open('utils\\users.sql', "r").read())
+        cursor.execute(open('utils\\produce.sql', "r").read())
+        cursor.close()
+    conn2.commit()
+    conn2.close()
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
